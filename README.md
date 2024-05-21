eff9b3f4f01db4ff19c66cfcb70958ba0b2019aa
s0ru <apazgarcia23@outlook.es>
2024-05-21 23:14:04 +0200

    upload base project

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### A shader that tries to mimic a pseudo-realistic earth planet.
 
-All the textures used (day texture, night texture and specular map belong to [Ssolar System Scope](https://www.solarsystemscope.com/textures/) under CC Attribution 4.0 License)
+All the textures used (day texture, night texture and specular map belong to [Solar System Scope](https://www.solarsystemscope.com/textures/) under CC Attribution 4.0 License)
 
 [DEMO]()
---
+++ src/script.js
@@ -0,0 +1,74 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import earthVertexShader from './shaders/earth/vertex.glsl'
+import earthFragmentShader from './shaders/earth/fragment.glsl'
+
+// Debug
+const gui = new GUI()
+
+const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+const textureLoader = new THREE.TextureLoader()
+
+const earthGeometry = new THREE.SphereGeometry(2, 64, 64)
+const earthMaterial = new THREE.ShaderMaterial({
+    vertexShader: earthVertexShader,
+    fragmentShader: earthFragmentShader,
+    uniforms:
+    {
+    }
+})
+const earth = new THREE.Mesh(earthGeometry, earthMaterial)
+scene.add(earth)
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    pixelRatio: Math.min(window.devicePixelRatio, 2)
+}
+
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(sizes.pixelRatio)
+})
+
+const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 12
+camera.position.y = 5
+camera.position.z = 4
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(sizes.pixelRatio)
+renderer.setClearColor('#000011')
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    earth.rotation.y = elapsedTime * 0.1
+
+    controls.update()
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
